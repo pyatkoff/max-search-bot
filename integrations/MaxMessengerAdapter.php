556618e9c2b5510856ad28943df01112edd74055
pyatkoff <pavel@pyatkoff.com>
2026-08-23 22:50:30 +0200

    Add MAX messenger adapter

---
+++ integrations/MaxMessengerAdapter.php
@@ -0,0 +1,28 @@
+<?php
+require_once __DIR__ . '/../contracts/MessengerInterface.php';
+
+class MaxMessengerAdapter implements MessengerInterface
+{
+    private $send;
+    private $sendWithButtons;
+
+    public function __construct(?callable $send = null, ?callable $sendWithButtons = null)
+    {
+        $this->send = $send ?: static function ($chatId, string $text): bool {
+            return class_exists('MaxSearchApi') && (bool)MaxSearchApi::MaxSend($text, $chatId);
+        };
+        $this->sendWithButtons = $sendWithButtons ?: static function ($chatId, string $text, array $buttons): bool {
+            return class_exists('MaxSearchApi') && (bool)MaxSearchApi::MaxSendWithButtons($text, $chatId, $buttons);
+        };
+    }
+
+    public function send($chatId, string $text): bool
+    {
+        return (bool)call_user_func($this->send, $chatId, $text);
+    }
+
+    public function sendWithButtons($chatId, string $text, array $buttons): bool
+    {
+        return (bool)call_user_func($this->sendWithButtons, $chatId, $text, $buttons);
+    }
+}
