fec848fb1ce160c07a7531c25e73ee2f0520f768
pyatkoff <pavel@pyatkoff.com>
2026-08-23 22:42:14 +0200

    Add versioned AnyTour project configuration

---
+++ project.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Versioned, non-secret project configuration.
+ * Secrets remain in config.php. This file describes only project identity,
+ * integration choices and public/non-secret routing settings.
+ */
+return [
+    'id' => 'anytour',
+    'brand' => [
+        'name' => 'AnyTour',
+    ],
+    'messenger' => [
+        'provider' => 'max',
+        'channel_url' => 'https://max.ru/anytour',
+        'miniapp_bot_url' => 'https://max.ru/id9704048781_2_bot',
+    ],
+    'search' => [
+        'provider' => 'tourvisor',
+        'base_domain' => 'https://anytour.online',
+        'claim_path' => '/poisk-turov-tg/{code}/',
+    ],
+    'leads' => [
+        'provider' => 'bitrix',
+        'claim_hl' => 33,
+        'iblock_id' => 4,
+        'section_id' => 26,
+        'status_id' => 9,
+        'uon_source_id' => 36,
+    ],
+    'state' => [
+        'legacy_hl' => 32,
+        'v2_store_dir' => 'runtime/trip_state',
+    ],
+    'analytics' => [
+        'provider' => 'metrika',
+    ],
+];
