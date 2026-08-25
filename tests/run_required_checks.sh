3972cdf7a316b20b0cc5b70ff8c939ed667657fe
pyatkoff <pavel@pyatkoff.com>
2026-08-26 01:42:55 +0200

    Run handoff integrity regression

--- tests/run_required_checks.sh
+++ tests/run_required_checks.sh
@@ -36,6 +36,7 @@ checks=(
   'php tests/run_manager_response_health_regression.php'
   'php tests/run_manager_push_health_regression.php'
   'php tests/run_manager_priority_regression.php'
+  'php tests/run_handoff_integrity_health_regression.php'
   'php tests/run_live_session_analyzer_regression.php'
   'php tests/run_live_session_snapshot_regression.php'
   'php tests/run_live_destination_context_regression.php'
