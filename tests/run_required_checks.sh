773ba613f970c48eb7a5d2640172b7f8225de5ab
pyatkoff <pavel@pyatkoff.com>
2026-08-25 11:51:04 +0200

    Run manager delivery panel regression

--- tests/run_required_checks.sh
+++ tests/run_required_checks.sh
@@ -32,6 +32,7 @@ checks=(
   'php tests/run_manager_visibility_regression.php'
   'php tests/run_manager_delivery_failure_regression.php'
   'php tests/run_manager_delivery_snapshot_regression.php'
+  'php tests/run_manager_delivery_panel_regression.php'
   'php tests/run_live_session_analyzer_regression.php'
   'php tests/run_post_tour_regression.php'
   'php tests/run_messenger_neutral_handlers_regression.php'
