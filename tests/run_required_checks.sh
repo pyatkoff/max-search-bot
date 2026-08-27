273b80b71a5e83386819bf1893fa353eeab34e51
pyatkoff <pavel@pyatkoff.com>
2026-08-27 20:08:28 +0300

    Run manager send guard regression in required CI

--- tests/run_required_checks.sh
+++ tests/run_required_checks.sh
@@ -50,6 +50,7 @@ checks=(
   'php tests/run_handoff_integrity_health_regression.php'
   'php tests/run_manager_assignment_integrity_regression.php'
   'php tests/run_manager_media_regression.php'
+  'php tests/run_manager_send_guard_regression.php'
   'php tests/run_sales_pipeline_foundation_regression.php'
   'php tests/run_manager_workspace_v2_regression.php'
   'php tests/run_manager_workspace_v2_wait_urgency_regression.php'
