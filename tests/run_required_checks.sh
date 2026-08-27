742f2488ece3e19ef72d82fae849e87f54ac6b49
pyatkoff <pavel@pyatkoff.com>
2026-08-27 20:37:48 +0300

    Guard visual QA workflow in required CI

--- tests/run_required_checks.sh
+++ tests/run_required_checks.sh
@@ -10,6 +10,7 @@ find . -type f -name '*.php' -not -path './vendor/*' -print0 | xargs -0 -n1 php 
 checks=(
   'php tests/run_autopilot_operating_contract_regression.php'
   'php tests/run_autopilot_snapshot_regression.php'
+  'php tests/run_workspace_v2_visual_qa_contract_regression.php'
   'php tests/run_regression.php'
   'php tests/run_live_september_typo_regression.php'
   'php tests/run_ai_context_regression.php'
