07b0822044b29c60ed6b8308d29a1d89c68437f6
pyatkoff <pavel@pyatkoff.com>
2026-08-29 13:03:22 +0300

    Expose precise source admin save errors (#315)
    * Harden source save error classification
    
    * Expose source save validation errors
    
    * Show specific source admin errors
    
    * Add source admin error contract regression
    
    * Fix source admin regression literals
    
    * Require source admin error regression

--- tests/required_checks_manifest.php
+++ tests/required_checks_manifest.php
@@ -70,6 +70,7 @@ return [
         'php tests/run_manager_phone_fallback_regression.php',
         'php tests/run_manager_visibility_regression.php',
         'php tests/run_manager_admin_audit_regression.php',
+        'php tests/run_manager_source_admin_errors_regression.php',
         'php tests/run_manager_delivery_failure_regression.php',
         'php tests/run_manager_delivery_snapshot_regression.php',
         'php tests/run_manager_delivery_panel_regression.php',
