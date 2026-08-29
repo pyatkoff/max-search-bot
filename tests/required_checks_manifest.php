567c7b2aed7f9aad937c652c3a6059cd0133fb6d
pyatkoff <pavel@pyatkoff.com>
2026-08-30 00:09:35 +0300

    Require inactive manager request monitoring regression

--- tests/required_checks_manifest.php
+++ tests/required_checks_manifest.php
@@ -122,6 +122,7 @@ return [
         'php tests/run_deploy_diagnostics_contract_regression.php',
         'php tests/run_production_snapshot_encoding_regression.php',
         'php tests/run_live_session_analyzer_regression.php',
+        'php tests/run_live_inactive_manager_request_regression.php',
         'php tests/run_live_calendar_callback_noise_regression.php',
         'php tests/run_live_successful_date_reselection_regression.php',
         'php tests/run_live_session_snapshot_regression.php',
