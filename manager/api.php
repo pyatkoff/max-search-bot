8562df29df4b06c8e3b914aa338f06ee266947d3
pyatkoff <pavel@pyatkoff.com>
2026-09-24 00:10:36 +0200

    feat: expose manager edit candidates

--- manager/api.php
+++ manager/api.php
@@ -130,6 +130,10 @@ if($action==='send'){
     $failure=ManagerOutboundService::lastFailure();
     out(['ok'=>false,'error'=>'delivery_failed','failure'=>$failure,'error_message'=>$failure?ManagerOutboundService::failureNotice($failure):'Сообщение не доставлено'],409);
 }
+if($action==='edit_candidates'){
+    $conversationId=(int)($data['conversation_id']??0);
+    out(['ok'=>true,'messages'=>ManagerMessageEditService::editableForConversation($conversationId,(int)$m['id'])]);
+}
 if($action==='edit_message'){
     $r=ManagerMessageEditService::edit((int)($data['message_id']??0),(int)$m['id'],(string)($data['text']??''));
     if(!empty($r['ok'])) out($r);
