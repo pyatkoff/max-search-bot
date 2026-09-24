67457bcc7ac93427961272c7e8d3240a5052c6b1
pyatkoff <pavel@pyatkoff.com>
2026-09-24 04:08:18 +0200

    Do not filter site search for any-stars choice

--- services/TourSearchHandoffService.php
+++ services/TourSearchHandoffService.php
@@ -19,7 +19,7 @@ final class TourSearchHandoffService
             'count_people' => self::positiveInt($savedData[$statusMap['adults']] ?? 0),
             'child_count' => $children,
             'child_age' => self::childAgeValues($savedData[$statusMap['child_ages']] ?? '', $children),
-            'stars' => self::positiveInt($savedData[$statusMap['stars']] ?? 0),
+            'stars' => self::searchStarsFilterValue($savedData[$statusMap['stars']] ?? 0),
             'food' => self::searchMealFilterValue($savedData[$statusMap['meal']] ?? 0),
             'yclid' => $yclid,
         ];
@@ -41,7 +41,7 @@ final class TourSearchHandoffService
             'count_people' => self::positiveInt($claim['UF_ADULTS'] ?? 0),
             'child_count' => $children,
             'child_age' => self::childAgeValues($claim['UF_AGE'] ?? '', $children),
-            'stars' => self::positiveInt($claim['UF_STARS'] ?? 0),
+            'stars' => self::searchStarsFilterValue($claim['UF_STARS'] ?? 0),
             'food' => self::searchMealFilterValue($claim['UF_MEAL'] ?? 0),
             'yclid' => $yclid,
         ];
@@ -51,6 +51,12 @@ final class TourSearchHandoffService
     {
         $value = (int)$value;
         return $value > 0 ? $value : 0;
+    }
+
+    private static function searchStarsFilterValue($value): int
+    {
+        $stars = self::positiveInt($value);
+        return $stars === 1 ? 0 : $stars;
     }
 
     private static function searchMealFilterValue($value): int
